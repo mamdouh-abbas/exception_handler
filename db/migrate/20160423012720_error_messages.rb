af2802123b54402dfcf9a81c95389b4741f73c55
madouh <tin.tin.nagi@gmail.com>
2016-04-30 22:47:29 +0200

    first commit

---
+++ db/migrate/20160423012720_error_messages.rb
@@ -0,0 +1,18 @@
+class ErrorMessages < ActiveRecord::Migration
+  def change
+  	 create_table :error_messages do |t|
+      t.text :class_name
+      t.text :message
+      t.text :trace
+      t.text :params
+      t.text :target_url
+      t.text :referer_url
+      t.text :user_agent
+      t.string :user_info
+      t.string :app_name
+      t.string :doc_root
+
+      t.timestamps
+    end
+  end
+end
